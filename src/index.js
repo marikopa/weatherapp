3f8d9006ec21bf9a3f00609e0db8f3f55cde61ff
marikopa <mariko.passel@gmail.com>
2021-04-14 15:25:52 +0300

    fixed apiUrl

--- src/index.js
+++ src/index.js
@@ -83,8 +83,7 @@ navigator.geolocation.getCurrentPosition(retrievePosition);
 
 function getForecast(coordinates) {
   let apiKey = "b05a1145e720875676132ce7411f570e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
